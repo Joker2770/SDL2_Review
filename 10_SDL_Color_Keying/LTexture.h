c0e9df311e9c08d0b21e8f86a3757a7a6f97ca4f
Joker2770 <1214220480@qq.com>
2020-08-30 10:44:40 +0800

    style: rename directories and change dependencies. :art:

---
+++ 10_SDL_Color_Keying/LTexture.h
@@ -0,0 +1,59 @@
+#ifndef __LTEXTURE_H__
+#define __LTEXTURE_H__
+
+#ifdef _WIN32
+//Windows
+extern "C"
+{
+#include "SDL.h"
+#include "SDL_image.h"
+};
+#else
+//Linux...
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#ifdef __cplusplus
+};
+#endif
+#endif
+
+#include <iostream>
+
+//Texture wrapper class
+class LTexture
+{
+public:
+	//Initializes variables
+	LTexture();
+
+	//Deallocates memory
+	~LTexture();
+
+	//Loads image at specified path
+	bool loadFromFile(std::string path);
+
+	//Deallocates texture
+	void free();
+
+	//Renders texture at given point
+	void render(int x, int );
+
+	//Gets image dimensions
+	int getWidth();
+	int getHeight();
+
+private:
+	//The actual hardware texture
+	SDL_Texture* mTexture;
+
+	//Image dimensions
+	int mWidth;
+	int mHeight;
+};
+
+#endif
