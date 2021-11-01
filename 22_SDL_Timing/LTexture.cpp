accd258ba651a397b314c2fcc3267f0a1076e49c
Joker2770 <1214220480@qq.com>
2021-11-01 13:58:38 +0800

    add 'timing' related.

---
+++ 22_SDL_Timing/LTexture.cpp
@@ -0,0 +1,339 @@
+#include "LTexture.h"
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+//���ڿ���
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void closeAll();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Globally used font
+TTF_Font *gFont = NULL;
+
+//Rendered texture
+//LTexture gTextTexture;
+LTexture gTimeTextTexture;
+LTexture gPromptTextTexture;
+
+LTexture::LTexture()
+{
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+	//Deallocate
+	free();
+}
+
+// bool LTexture::loadFromFile(std::string path)
+// {
+// 	//Get rid of preexisting texture
+// 	free();
+// 
+// 	//The final texture
+// 	SDL_Texture* newTexture = NULL;
+// 
+// 	//Load image at specified path
+// 	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+// 	if (loadedSurface == NULL)
+// 	{
+// 		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+// 	}
+// 	else
+// 	{
+// 		//Color key image
+// 		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+// 		//Create texture from surface pixels
+// 		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+// 		if (newTexture == NULL)
+// 		{
+// 			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+// 		}
+// 		else
+// 		{
+// 			//Get image dimensions
+// 			mWidth = loadedSurface->w;
+// 			mHeight = loadedSurface->h;
+// 		}
+// 
+// 		//Get rid of old loaded surface
+// 		SDL_FreeSurface(loadedSurface);
+// 	}
+// 
+// 	//Return success
+// 	mTexture = newTexture;
+// 	return mTexture != NULL;
+// }
+
+bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
+	if (textSurface == NULL)
+	{
+		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+	}
+	else
+	{
+		//Create texture from surface pixels
+		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+		if (mTexture == NULL)
+		{
+			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		//Get rid of old surface
+		SDL_FreeSurface(textSurface);
+	}
+
+	//Return success
+	return mTexture != NULL;
+}
+
+void LTexture::free()
+{
+	//Free texture if it exists
+	if (mTexture != NULL)
+	{
+		SDL_DestroyTexture(mTexture);
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+}
+
+void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+	//Set clip rendering dimensions
+	if (clip != NULL)
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	//Render to screen
+	SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
+}
+
+int LTexture::getWidth()
+{
+	return mWidth;
+}
+
+int LTexture::getHeight()
+{
+	return mHeight;
+}
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+
+		//Create window
+		gWindow = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+// 				//Initialize PNG loading
+// 				int imgFlags = IMG_INIT_PNG;
+// 				if (!(IMG_Init(imgFlags) & imgFlags))
+// 				{
+// 					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+// 					success = false;
+// 				}
+
+				//Initialize SDL_ttf
+				if (TTF_Init() == -1)
+				{
+					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+					success = false;
+				}
+			}
+
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+    //Open the font
+    gFont = TTF_OpenFont( "22_timing/lazy.ttf", 28 );
+    if( gFont == NULL )
+    {
+        printf( "Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Set text color as black
+        SDL_Color textColor = { 0, 0, 0, 255 };
+        
+        //Load prompt texture
+        if( !gPromptTextTexture.loadFromRenderedText( "Press Enter to Reset Start Time.", textColor ) )
+        {
+            printf( "Unable to render prompt texture!\n" );
+            success = false;
+        }
+    }
+
+	return success;
+}
+
+void closeAll()
+{
+	//Free loaded images
+	gPromptTextTexture.free();
+	gTimeTextTexture.free();
+
+	//Free global font
+	TTF_CloseFont(gFont);
+	gFont = NULL;
+
+	//Destroy window    
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	TTF_Quit();
+//	IMG_Quit();
+	SDL_Quit();
+}
+
+int main(int argc, char *argv[])
+{
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+            //Set text color as black
+            SDL_Color textColor = { 0, 0, 0, 255 };
+
+            //Current time start time
+            Uint32 startTime = 0;
+
+            //In memory text stream
+            std::stringstream timeText;
+
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+                    //Reset start time on return keypress
+                    else if( e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN )
+                    {
+                        startTime = SDL_GetTicks();
+                    }
+				}
+				
+				//Set text to be rendered
+                timeText.str( "" );
+                timeText << "Milliseconds since start time " << SDL_GetTicks() - startTime;
+				
+				//Render text
+                if( !gTimeTextTexture.loadFromRenderedText( timeText.str().c_str(), textColor ) )
+                {
+                    printf( "Unable to render time texture!\n" );
+                }
+				
+				//Clear screen
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                SDL_RenderClear( gRenderer );
+
+                //Render textures
+                gPromptTextTexture.render( ( SCREEN_WIDTH - gPromptTextTexture.getWidth() ) / 2, 0 );
+                gTimeTextTexture.render( ( SCREEN_WIDTH - gPromptTextTexture.getWidth() ) / 2, ( SCREEN_HEIGHT - gPromptTextTexture.getHeight() ) / 2 );
+
+                //Update screen
+                SDL_RenderPresent( gRenderer );
+			}
+		}
+	}
+	closeAll();
+
+	return 0;
+}
