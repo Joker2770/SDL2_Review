2af4e85699b52b3d21b83467944f9aeab1932a8c
Joker2770 <1214220480@qq.com>
2021-11-26 09:23:05 +0800

    feat: add 'Collision Detection'. :arrow_up:

---
+++ 27_SDL_Collision_Detection/Dot.cpp
@@ -0,0 +1,137 @@
+/*************************************************************************
+    > File Name: Dot.cpp
+    > Author: Jintao Yang
+    > Mail: 18608842770@163.com 
+    > Created Time: 2021年11月17日 星期三 10时54分31秒
+ ************************************************************************/
+
+#include "Dot.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Box collision detector
+bool checkCollision( SDL_Rect a, SDL_Rect b );
+
+bool checkCollision( SDL_Rect a, SDL_Rect b )
+{
+    //The sides of the rectangles
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    //Calculate the sides of rect A
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    //Calculate the sides of rect B
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;    
+    
+    //If any of the sides from A are outside of B
+    if( bottomA <= topB )
+    {
+        return false;
+    }
+
+    if( topA >= bottomB )
+    {
+        return false;
+    }
+
+    if( rightA <= leftB )
+    {
+        return false;
+    }
+
+    if( leftA >= rightB )
+    {
+        return false;
+    }
+
+    //If none of the sides from A are outside B
+    return true;
+}
+
+Dot::Dot()
+{
+    //Initialize the offsets
+    mPosX = 0;
+    mPosY = 0;
+
+    //Set collision box dimension
+    mCollider.w = DOT_WIDTH;
+    mCollider.h = DOT_HEIGHT;
+
+    //Initialize the velocity
+    mVelX = 0;
+    mVelY = 0;
+}
+
+void Dot::handleEvent( SDL_Event& e )
+{
+    //If a key was pressed
+    if( e.type == SDL_KEYDOWN && e.key.repeat == 0 )
+    {
+        //Adjust the velocity
+        switch( e.key.keysym.sym )
+        {
+            case SDLK_UP: mVelY -= DOT_VEL; break;
+            case SDLK_DOWN: mVelY += DOT_VEL; break;
+            case SDLK_LEFT: mVelX -= DOT_VEL; break;
+            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+        }
+    }
+    
+    //If a key was released
+    else if( e.type == SDL_KEYUP && e.key.repeat == 0 )
+    {
+        //Adjust the velocity
+        switch( e.key.keysym.sym )
+        {
+            case SDLK_UP: mVelY += DOT_VEL; break;
+            case SDLK_DOWN: mVelY -= DOT_VEL; break;
+            case SDLK_LEFT: mVelX += DOT_VEL; break;
+            case SDLK_RIGHT: mVelX -= DOT_VEL; break;
+        }
+    }
+}
+
+void Dot::move( SDL_Rect& wall )
+{
+    //Move the dot left or right
+    mPosX += mVelX;
+    mCollider.x = mPosX;
+
+    //If the dot went too far to the left or right
+    if( ( mPosX < 0 ) || ( mPosX + DOT_WIDTH > SCREEN_WIDTH ) || checkCollision( mCollider, wall ) )
+    {
+        //Move back
+        mPosX -= mVelX;
+        mCollider.x = mPosX;
+    }
+    
+    //Move the dot up or down
+    mPosY += mVelY;
+    mCollider.y = mPosY;
+
+    //If the dot went too far up or down
+    if( ( mPosY < 0 ) || ( mPosY + DOT_HEIGHT > SCREEN_HEIGHT ) || checkCollision( mCollider, wall ) )
+    {
+        //Move back
+        mPosY -= mVelY;
+        mCollider.y = mPosY;
+    }
+}
+
+void Dot::render(SDL_Renderer* renderer, LTexture *gDotTexture)
+{
+    //Show the dot
+    gDotTexture->render(renderer, mPosX, mPosY );
+}
+
---
+++ 27_SDL_Collision_Detection/Dot.h
@@ -0,0 +1,50 @@
+/*************************************************************************
+    > File Name: Dot.h
+    > Author: Jintao Yang
+    > Mail: 18608842770@163.com 
+    > Created Time: 2021年11月17日 星期三 10时54分17秒
+ ************************************************************************/
+
+#ifndef __DOT_H__
+#define __DOT_H__
+
+#include "LTexture.h"
+#include<iostream>
+using namespace std;
+
+//The dot that will move around on the screen
+class Dot
+{
+    public:
+        //The dimensions of the dot
+        static const int DOT_WIDTH = 20;
+        static const int DOT_HEIGHT = 20;
+
+        //Maximum axis velocity of the dot
+        static const int DOT_VEL = 1;
+
+        //Initializes the variables
+        Dot();
+
+        //Takes key presses and adjusts the dot's velocity
+        void handleEvent( SDL_Event& e );
+
+        //Moves the dot
+        void move( SDL_Rect& wall );
+
+        //Shows the dot on the screen
+        void render(SDL_Renderer* Renderer, LTexture *gDotTexture);
+
+    private:
+        //The X and Y offsets of the dot
+        int mPosX, mPosY;
+
+        //The velocity of the dot
+        int mVelX, mVelY;
+        
+        //Dot's collision box
+        SDL_Rect mCollider;
+};
+
+#endif
+
