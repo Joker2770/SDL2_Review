3c4a77df8288a182bdc945965c099bf5d1a24583
Joker2770 <1214220480@qq.com>
2022-01-05 15:31:49 +0800

    remove unrelated. :arrow_down:

--- 32_SDL_Text_Input_And_Clipboard_Handling/config.h
+++ 32_SDL_Text_Input_And_Clipboard_Handling/config.h
@@ -32,10 +32,6 @@ extern "C" {
 #endif
 #endif
 
-//The dimensions of the level
-const int LEVEL_WIDTH = 1280;
-const int LEVEL_HEIGHT = 960;
-
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_FPS = 60;
